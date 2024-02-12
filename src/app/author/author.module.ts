d849fa61a817b011340615bf56f41f57bc9a76e4
dongnez <jimenezjimenezguillermo@gmail.com>
2024-02-12 09:47:15 +0100

    listado paginado

--- src/app/author/author.module.ts
+++ src/app/author/author.module.ts
@@ -2,6 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthorListComponent } from './author-list/author-list.component';
 import { AuthorEditComponent } from './author-edit/author-edit.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
 
 
 
@@ -11,7 +19,22 @@ import { AuthorEditComponent } from './author-edit/author-edit.component';
     AuthorEditComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatTableModule,
+    MatIconModule, 
+    MatButtonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatPaginatorModule,
+  ],
+  providers:[
+    {
+      provide: MAT_DIALOG_DATA,
+      useValue: {}
+    }
   ]
 })
 export class AuthorModule { }
--- src/app/author/author.service.ts
+++ src/app/author/author.service.ts
@@ -1,26 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Pageable } from '../core/model/page/Pageable';
 import { Author } from './model/Author';
 import { AuthorPage } from './model/AuthorPage';
-import { AUTHOR_DATA } from './model/mock-authors';
 
 @Injectable({
-    providedIn: 'root'
+providedIn: 'root'
 })
 export class AuthorService {
 
-    constructor() { }
+    constructor(
+        private http: HttpClient
+    ) { }
 
     getAuthors(pageable: Pageable): Observable<AuthorPage> {
-        return of(AUTHOR_DATA);
+        return this.http.post<AuthorPage>('http://localhost:8080/author', {pageable:pageable});
     }
 
     saveAuthor(author: Author): Observable<void> {
-        return of(null);
+        let url = 'http://localhost:8080/author';
+        if (author.id != null) url += '/'+author.id;
+
+        return this.http.put<void>(url, author);
     }
 
     deleteAuthor(idAuthor : number): Observable<void> {
-        return of(null);
+        return this.http.delete<void>('http://localhost:8080/author/'+idAuthor);
     }    
 }
